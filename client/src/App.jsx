5e3aa9116c3a4392573273b7d6bf67d8202f27a9
tanujasaste <tanujasaste9.com>
2025-06-27 19:35:48 +0530

    Added ProfileChecker

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,13 @@ import AuthDashboard from "./pages/AuthDashboard";
 import Dashboard from "./pages/Dashboard";
 import ProfileChecker from "./contextes/ProfileChecker";
 import CompleteProfile from "./pages/CompleteProfile";
-import UserProfile from "./pages/UserProfile";
+// import UserProfile from "./pages/UserProfile";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<AuthDashboard />} />
-      <Route path="/profile" element={<UserProfile/>} />
+      {/* <Route path="/profile" element={<UserProfile/>} /> */}
       <Route path="/complete-profile" element={<CompleteProfile />} />
       
       {/* Protect dashboard route */}
